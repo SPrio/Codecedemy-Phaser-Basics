6fc5112d822670ce0d62c55770e6da74b791a71e
SPrio <mahantasupriyo@gmail.com>
2021-01-13 19:35:43 +0530

    Move Your Bodies

---
+++ game_7.js
@@ -0,0 +1,25 @@
+let codey;
+
+function preload() {
+  this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png');
+}
+
+function create() {
+  codey = this.add.sprite(30, 200, 'codey')
+}
+
+// Create your update() function here
+
+const config = {
+	type: Phaser.AUTO,
+	width: 400,
+	height: 400,
+	backgroundColor: "#5f2a55",
+	scene: {
+    preload,
+    create,
+    // Include update here!
+	}
+}
+
+const game = new Phaser.Game(config)
