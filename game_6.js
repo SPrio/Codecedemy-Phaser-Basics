99341770994cceace2f6b95f1a582f0011ab2d23
SPrio <mahantasupriyo@gmail.com>
2021-01-13 19:34:51 +0530

    add scene with create in config of game_6

--- game_6.js
+++ game_6.js
@@ -8,7 +8,9 @@ const config = {
 	height: 600,
 	backgroundColor: "#a0a0dd",
   // Add in the scene information in the config here:
-
+  scene: {
+    create
+  }
 }
 
 const game = new Phaser.Game(config)
