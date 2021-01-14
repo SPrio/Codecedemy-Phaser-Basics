0f07f58c3f0137fe568762be274ee4a3dceaa7b5
SPrio <mahantasupriyo@gmail.com>
2021-01-14 11:18:30 +0530

    added keyboard events

--- game.10.js
+++ game.10.js
@@ -7,12 +7,25 @@ function preload() {
 function create() {
   gameState.codey = this.add.sprite(150, 200, 'codey')
   // Set cursor keys here!
-
+  gameState.cursors = this.input.keyboard.createCursorKeys()
+  gameState.speed = 1
 }
 
 function update() {
   // Update based on keypress here!
-
+ if(gameState.cursors.right.isDown){
+   gameState.speed+=1
+   gameState.codey.x+=gameState.speed
+ }
+ if(gameState.cursors.left.isDown){
+   gameState.codey.x-=5
+ }
+ if(gameState.cursors.up.isDown){
+   gameState.codey.y-=5
+ }
+ if(gameState.cursors.down.isDown){
+   gameState.codey.y+=5
+ }
 }
 
 const config = {
