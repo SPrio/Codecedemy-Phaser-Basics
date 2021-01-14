5ca2f31188f255a3d1dd0dd8042d9473d64bf044
SPrio <mahantasupriyo@gmail.com>
2021-01-14 18:21:19 +0530

    [Game 12] Added sprite in create()

--- game_12.js
+++ game_12.js
@@ -1,11 +1,15 @@
 const gameState = {};
 
-preload = () => {
+function preload() {
   this.load.image(
     "codey",
     "https://content.codecademy.com/courses/learn-phaser/codey.png"
   );
-};
+}
+
+function create() {
+  gameState.codey = this.add.sprite(100, 100, "codey");
+}
 
 const config = {
   width: 400,
