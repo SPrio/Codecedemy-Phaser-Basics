b6e021b8890a3fc0c9e25ece553a0ecc406b9bf3
SPrio <mahantasupriyo@gmail.com>
2021-01-14 12:58:11 +0530

    Added sound on mouse click game_11

--- game_11.js
+++ game_11.js
@@ -1,30 +1,46 @@
-const gameState = {}
+const gameState = {};
 
 function preload() {
   // load our 'incredible' sound here!
 
-    this.load.audio('awesome', 'https://content.codecademy.com/courses/learn-phaser/reallyawesome.mp3')
+  this.load.audio(
+    "awesome",
+    "https://content.codecademy.com/courses/learn-phaser/reallyawesome.mp3"
+  );
 
+  this.load.audio(
+    "incredible",
+    "https://content.codecademy.com/courses/learn-phaser/incredible.mp3"
+  );
 }
 
 function create() {
   // add our 'incredible' sound to our scene here!
 
-  gameState.awesome = this.sound.add('awesome')
-
+  gameState.awesome = this.sound.add("awesome");
+  gameState.incredible = this.sound.add("incredible");
   // Display "Incredible" and "Really, really awesome" buttons
-
-  gameState.awesomeBox = this.add.rectangle(200, 400, 300, 200, 0xaadada)
-  gameState.incredibleText = this.add.text(150, 135, "Incredible", { fill: "#222222", font: "20px Times New Roman"})
-  gameState.awesomeText = this.add.text(110, 385, "Really, really awesome", { fill: "#222222", font: "20px Times New Roman"})
+  gameState.incredibleBox = this.add.rectangle(200, 150, 300, 200, 0xdadaaa);
+  gameState.awesomeBox = this.add.rectangle(200, 400, 300, 200, 0xaadada);
+  gameState.incredibleText = this.add.text(150, 135, "Incredible", {
+    fill: "#222222",
+    font: "20px Times New Roman",
+  });
+  gameState.awesomeText = this.add.text(110, 385, "Really, really awesome", {
+    fill: "#222222",
+    font: "20px Times New Roman",
+  });
   gameState.incredibleBox.setInteractive();
   gameState.awesomeBox.setInteractive();
 
   // add a 'pointerup' handler to incredibleBox here:
+  gameState.incredibleBox.on("pointerup", function () {
+    gameState.incredible.play();
+  });
 
-  gameState.awesomeBox.on('pointerup', function() {
-    gameState.awesome.play()
-  })
+  gameState.awesomeBox.on("pointerup", function () {
+    gameState.awesome.play();
+  });
 }
 
 const config = {
@@ -34,8 +50,8 @@ const config = {
   backgroundColor: "#333333",
   scene: {
     preload,
-    create
-  }
-}
+    create,
+  },
+};
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
